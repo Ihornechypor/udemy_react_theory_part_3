85f6b90a22f4dd537441cca413c7d8c9d07029f4
ihor.nechypor <ihor.nechypor@plej.pl>
2020-07-21 14:18:58 +0200

    lesson 68 render element if logged in

--- src/App.js
+++ src/App.js
@@ -7,47 +7,65 @@ import CarDetail from './CarDetail/CarDetail'
 import h1 from "eslint-plugin-jsx-a11y/lib/util/implicitRoles/h1";
 
 class App extends Component {
-  render() {
+    state = {
+        isLoggedIn: false
+    }
 
-    return (
-      <div>
-        <nav className="nav">
-          <ul>
-                <li>
-                  <NavLink
-                      to="/"
-                      exact
-                      activeClassName={'wft-active'}>Home</NavLink>
-                </li>
-                <li>
-                  <NavLink
-                      to="/about"
-                      activeStyle={{
-                           color: 'blue'
-                      }}
-                  >About</NavLink>
-                </li>
-              <li>
-                  <NavLink to={{
-                      pathname: '/cars'
-                  }}>cars</NavLink>
-              </li>
-          </ul>
-        </nav>
+    render() {
 
-        <hr/>
-        <Switch>
-            <Route path="/" exact render={() => <h1>home page</h1>}/>
-            <Route path="/about" component={About}/>
-            <Route path="/cars/:name" component={CarDetail}/>
-            <Route path="/cars" component={Cars}/>
-            <Redirect to={'/'}/>
-            {/*<Route render={() => <h1 style={{color:'red', textAlign: 'center'}}>404</h1>}/>*/}
-        </Switch>
+        return (
+            <div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/"
+                                exact
+                                activeClassName={'wft-active'}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/about"
+                                activeStyle={{
+                                    color: 'blue'
+                                }}
+                            >About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={{
+                                pathname: '/cars'
+                            }}>cars</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <hr/>
+                <div>
+                    <h3>
+                        dd {this.state.isLoggedIn ? 'true' : 'false'}
+                    </h3>
 
-      </div>
-    );
-  }
+                    <button onClick={() => this.setState({isLoggedIn: true})}>
+                        log in
+                    </button>
+                </div>
+
+
+                <hr/>
+                <Switch>
+                    <Route path="/" exact render={() => <h1>home page</h1>}/>
+                    {
+                        this.state.isLoggedIn ? <Route path="/about" component={About}/> : false
+                    }
+
+                    <Route path="/cars/:name" component={CarDetail}/>
+                    <Route path="/cars" component={Cars}/>
+                    <Redirect to={'/'}/>
+                    {/*<Route render={() => <h1 style={{color:'red', textAlign: 'center'}}>404</h1>}/>*/}
+                </Switch>
+
+            </div>
+        );
+    }
 }
 
 export default App
